18ccd667b7380e97ce49ce19bda2c777a3f56e20
MrVaxiZ <vaxizbe@gmail.com>
2023-04-11 22:34:47 +0200

    Added Loading screen, possibility to edit profile

--- DA/client/src/app/_modules/shared.module.ts
+++ DA/client/src/app/_modules/shared.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     NgxGalleryModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule.forRoot({ type: 'pacman' }),
     TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
@@ -20,7 +24,8 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
     BsDropdownModule,
     ToastrModule,
     TabsModule,
-    NgxGalleryModule
+    NgxGalleryModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
