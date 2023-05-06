62691a2c1685fbfd99560f0807ba37bb0addd11c
MrVaxiZ <vaxizbe@gmail.com>
2023-05-06 14:54:58 +0200

    SignalR for Messages and impoved queries

--- DA/API/SignalR/PresenceHub.cs
+++ DA/API/SignalR/PresenceHub.cs
@@ -20,21 +20,23 @@ namespace API.SinalR
 
         public override async Task OnConnectedAsync()
         {
-            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
-            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+            var IsOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+            if(IsOnline)
+            {
+                await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+            }
 
             var currentUsers = await _tracker.GetOnlineUsers();
-            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+            await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
-
-            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
-
-            var currentUsers = await _tracker.GetOnlineUsers();
-            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+            var IsOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
+            if(IsOffline)
+            {
+                await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+            }
 
             await base.OnDisconnectedAsync(exception);
         }
